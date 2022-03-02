192eee0ffcaf78ddfc11a5bd8cd58aa2094e9bb9
vilouie <zyxwvrl@gmail.com>
2022-03-02 01:48:27 -0800

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='BSA Research Team on Soccer and xG Statistic: public version',
+    author='Victor Louie',
+    license='MIT',
+)
